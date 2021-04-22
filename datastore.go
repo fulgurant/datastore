890f6db4283b4cb9499300a1e41cd76c3b6135ba
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2021-04-22 16:26:14 +0100

    Refactor

--- datastore.go
+++ datastore.go
@@ -10,8 +10,8 @@ var (
 // ListFunc is used to list items in the datastore
 type ListFunc func(key []byte, value []byte) error
 
-// GetSetter is used to get or set values in a datastore
-type GetSetter interface {
+// Handler is used to get or set values in a datastore
+type Handler interface {
 	Get(bucket []byte, key []byte) (value []byte, err error)
 	Set(bucket []byte, key []byte, value []byte) error
 	List(bucket []byte, pattern []byte, f ListFunc) error
